7f30953e050b279cbd837f4de1655c6f33926c99
Reversereven <a-tamagawa@aqte.net>
2019-01-19 14:59:05 +0900

    指摘箇所の修正
    これで完成

--- js/omikuji.js
+++ js/omikuji.js
@@ -1,20 +1,20 @@
 
+'use strict';
 
 document.getElementById("re").onclick = function(){
 
   var e = document.getElementById("btn");
-  var rslt =Math.floor(Math.random()*100);
+  var rand =Math.floor(Math.random()*100);
 
-  msg = "大吉";
-  if (rslt > 4) msg = "中吉";
-  if (rslt > 19) msg = "小吉";
-  if (rslt > 44) msg = "吉";
-  if (rslt > 74) msg = "末吉";
-  if (rslt > 89) msg = "凶";
+  var msg = "大吉";
+  if (rand > 4) msg = "中吉";
+  if (rand > 19) msg = "小吉";
+  if (rand > 44) msg = "吉";
+  if (rand > 74) msg = "末吉";
+  if (rand > 89) msg = "凶";
 
   e.textContent = (msg);
 }
-
 
 
 
